9281cb95d6b178ad0e01f213978f0e8ed9294c1b
abdalla440 <ahannora440@gmail.com>
2024-07-17 02:17:45 +0300

    initial push of the terraform code

---
+++ terraform/terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+  cloud {
+    organization = "A-Hannora-Systems"
+    workspaces {
+      name = "ramzy-task"
+    }
+  }
+
+  required_providers {
+    
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.56.1"
+    }
+    
+  }
+
+  required_version = ">= 1.2.0"
+}
+
